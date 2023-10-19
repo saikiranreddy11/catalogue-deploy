bedcecf5d93e5d9b2053fa61bce746dcca792b17
saikiranreddysudhi <www.saikiransudhireddy@gmail.com>
2023-10-19 16:10:23 -0400

    changed the version in script catalogue.sh

--- terraform/catalogue.sh
+++ terraform/catalogue.sh
@@ -5,11 +5,10 @@
 # pip3.11 install ansible botocore boto3
 # cd /tmp
 # ansible-pull -U https://github.com/saikiranreddy11/roboshop-ansible-roles-tf.git -e component = catalogue -e version = $package_version  main.yaml
-
 #!/bin/bash
 APP_VERSION=$1
 echo "app version: $APP_VERSION"
 yum install python3.11-devel python3.11-pip -y
 pip3.11 install ansible botocore boto3
 cd /tmp
-ansible-pull -U https://github.com/sivadevopsdaws74s/ansible-roboshop-roles-tf.git -e component=catalogue -e app_version=$APP_VERSION main.yaml
+ansible-pull -U https://github.com/saikiranreddy11/roboshop-ansible-roles-tf.git  -e component=catalogue -e app_version=$APP_VERSION main.yaml
