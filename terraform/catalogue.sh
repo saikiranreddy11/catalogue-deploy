a2db2c8ccbb80d115c80d35b8b1cc1a5d5e302df
saikiranreddysudhi <www.saikiransudhireddy@gmail.com>
2023-10-19 16:00:53 -0400

    changed the version in script catalogue.sh

--- terraform/catalogue.sh
+++ terraform/catalogue.sh
@@ -1,10 +1,15 @@
+# #!/bin/bash
+# package_version = "$1" 
+# echo "version is from shell script  $package_version"
+# yum install python3.11-devel python3.11-pip -y
+# pip3.11 install ansible botocore boto3
+# cd /tmp
+# ansible-pull -U https://github.com/saikiranreddy11/roboshop-ansible-roles-tf.git -e component = catalogue -e version = $package_version  main.yaml
+
 #!/bin/bash
-package_version = "$1" # to avaoid the ambiguity , variable name is changed to package version
-
-echo "version is from shell script  $package_version"
+APP_VERSION=$1
+echo "app version: $APP_VERSION"
 yum install python3.11-devel python3.11-pip -y
-
 pip3.11 install ansible botocore boto3
-
 cd /tmp
-ansible-pull -U https://github.com/saikiranreddy11/roboshop-ansible-roles-tf.git -e component = catalogue -e version = $package_version  main.yaml
+ansible-pull -U https://github.com/sivadevopsdaws74s/ansible-roboshop-roles-tf.git -e component=catalogue -e app_version=$APP_VERSION main.yaml
